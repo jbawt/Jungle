3fabd254f402e0e942785c8eb47b058677426609
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-10-15 20:36:02 +0000

    add category feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASS"]
+  
   def show
     @product = Product.count
     @category = Category.count
